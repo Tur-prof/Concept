61b225180ef03a6c567d9d59b466536f71d23f34
Tur-prof <tur_allex@mail.ru>
2023-10-04 11:58:33 +0700

    feat:

---
+++ main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <concepts>
+#include <string>
+
+template <typename T>
+concept ComplexConcept = requires(T val)
+{
+	{val.toString()} -> std::same_as<std::string>;
+	{val.hash()} -> std::convertible_to<long>;
+	std::has_virtual_destructor <T>::value;
+};
+
+class Check
+{
+	int n;
+public:
+	Check(int val) : n(val)
+	{
+		std::cout << "Constructor" << std::endl;
+	}
+	std::string toString() {
+		return std::to_string(n);
+	}
+	int hash() {
+		return n;
+	}
+	~Check() {
+		std::cout << "Distructor" << std::endl;
+	};
+};
+
+template <ComplexConcept T>
+void conceptMetod(T data)
+{
+	std::cout << "Metod toString: " << data.toString() << std::endl;
+	std::cout << "Metod hash: " << data.hash() << std::endl;
+}
+
+int main()
+{
+	Check test(10);
+	conceptMetod(test);
+	return 0;
+}
